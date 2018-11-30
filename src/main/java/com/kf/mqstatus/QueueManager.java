a4ab3c8735a7e197ed4aa8ddabfd8da820fefe42
shyamsunderkaranam <40713356+shyamsunderkaranam@users.noreply.github.com>
2018-11-30 21:36:03 +0530

    Final changes

--- src/main/java/com/kf/mqstatus/QueueManager.java
+++ src/main/java/com/kf/mqstatus/QueueManager.java
@@ -1,4 +1,5 @@
 package com.kf.mqstatus;
+import com.ibm.mq.jms.MQConnectionFactory;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -15,6 +16,7 @@ import java.util.Map;
 import com.ibm.mq.*;
 import com.ibm.mq.constants.CMQC;
 import com.ibm.mq.constants.CMQCFC;
+import com.ibm.mq.constants.MQConstants;
 import com.ibm.mq.pcf.MQCFST;
 import com.ibm.mq.pcf.PCFAgent;
 import com.ibm.mq.pcf.PCFException;
@@ -30,6 +32,7 @@ public class QueueManager {
 	private final String channel;
 	private final String manager;
 	private final MQQueueManager qmgr;
+	private MQConnectionFactory mqCF;
 
 	public QueueManager(String host, int port, String channel, String manager) throws MQException {
 		this.host = host;
@@ -552,5 +555,96 @@ public class QueueManager {
 		}
 
 		return flag;
+	}
+	
+	public ArrayList<String> getQueueGetStatus() throws MQException, Exception
+	{
+		int flag = 0 ;
+		MQQueue queue = null ;
+		boolean queueInhibitflag=false;
+		ArrayList<String> queuelist  = new ArrayList<String>();
+		String queueNames[]={"TEST","TEST1","TEST2","SALESORDER.ATG.INBOUND.WMB","SEQUENTIAL.ECCGENERIC.WMB.OUTBOUND.WMB","ZCRMXIF_PARTNER_SAVE01.CRM.INBOUND.LOCAL"};
+	
+		for(int i =0;i<queueNames.length;i++)
+		{
+			String queueName = queueNames[i];
+			queue = qmgr.accessQueue(queueName, MQC.MQOO_INQUIRE | MQC.MQOO_INPUT_AS_Q_DEF, null, null, null);
+			flag = queue.getInhibitGet();
+			if(flag==1) {
+				queuelist.add(queueNames[i]);
+				queueInhibitflag = true;
+			}//if(flag==1) {
+			else
+				System.out.println(queueName+" Queue is not inhibited\n");
+			if (queue != null)
+			{
+				try
+				{
+				queue.close() ;
+				}
+				catch (MQException ex)
+				{
+					System.out.println("ignoring error closing queue: " ) ;
+				}
+			}//if (queue != null)
+		}//For Loop
+	
+		if(queueInhibitflag == false) {
+			
+			System.out.println("\nNo Queues are inhibited");
+			queuelist.add("No Queues are inhibited");
+			
+		} //if(queueInhibitflag == false) {
+		
+		return queuelist;
+	}
+	public String removeGetInhibition(String queueName) throws Exception
+	{ 
+	MQQueue queue = null ;
+	int openOptions = MQConstants.MQOO_FAIL_IF_QUIESCING | MQConstants.MQOO_SET;
+	queue = getQueueManager().accessQueue(queueName, openOptions, null, null, null);
+	//int option1 = getQueueGetStatus();
+	//if (option1==1){
+	//System.out.println("Start mesage received");
+	queue.setInhibitGet(MQC.MQQA_GET_ALLOWED);
+	//System.out.println("Queue get allowed successfully");
+
+	//}
+	/*else {
+	            System.out.println("Stop mesage received");
+	            queue.setInhibitGet(MQC.MQQA_GET_INHIBITED);
+	            System.out.println("Queue inhibitted successfully");
+	        }*/
+	
+	return "SUCCESS";
+	}
+	
+	private synchronized MQQueueManager getQueueManager() throws Exception
+	{
+	if (queueManager == null)
+	{
+	MQEnvironment.channel = "MQPREPRDSUP.SVRCONN";
+
+	MQEnvironment.port = 1430;
+
+	MQEnvironment.hostname = "unxs0614.ghanp.kfplc.com";
+	System.out.println(MQEnvironment.hostname);
+
+	MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES);
+	if (mqCF.getSecurityExit() != null) {
+	Class clazz = getClass().getClassLoader().loadClass(mqCF.getSecurityExit()) ;
+	MQSecurityExit securityExit = null ;
+	if (mqCF.getSecurityExitInit() != null) {
+	securityExit = (MQSecurityExit) clazz.getConstructor(String.class).newInstance(mqCF.getSecurityExitInit()) ;
+	} else {
+	securityExit = (MQSecurityExit) clazz.newInstance() ;
+	}
+	MQEnvironment.securityExit = securityExit ;
+	}
+
+	queueManager = new MQQueueManager(mqCF.getQueueManager());
+	}
+
+	return queueManager;
 	}
 }
