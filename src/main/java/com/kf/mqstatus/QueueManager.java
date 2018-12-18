c934c477c04749440649314095995152a5b9297d
shyamsunderkaranam <40713356+shyamsunderkaranam@users.noreply.github.com>
2018-12-18 21:30:36 +0530

    All Queues fetch changes

--- src/main/java/com/kf/mqstatus/QueueManager.java
+++ src/main/java/com/kf/mqstatus/QueueManager.java
@@ -23,6 +23,9 @@ import com.ibm.mq.pcf.PCFException;
 import com.ibm.mq.pcf.PCFMessage;
 import com.ibm.mq.pcf.PCFMessageAgent;
 import com.ibm.mq.pcf.PCFParameter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 
 public class QueueManager {
 	
@@ -39,7 +42,9 @@ public class QueueManager {
 		this.port = port;
 		this.channel = channel;
 		this.manager = manager;
+		System.out.println("Host:"+host+" port:"+port+" channel:"+channel+" QManager:"+manager);
 		this.qmgr = createQueueManager();
+		System.out.println("Host:"+host+" port:"+port+" channel:"+channel+" QManager:"+manager);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -53,114 +58,10 @@ public class QueueManager {
 
 	/*public static void main(String args[]) throws MQException {
 		
-		QueueManager qm = new QueueManager("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN", "FMQINST1");
+		QueueManager qm=new QueueManager("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN", "FMQINST1");
+		System.out.println(qm.listOfQueues());
+		//qm.checkChannelStatus();
 
-		System.out.println(qm.depthOf("TEST"));
-		qm.checkChannelStatus();
-
-	}*/
-	
-	
-	/*public synchronized Map getStatistics() throws Exception {
-		// System.out.println("inside");
-		final Map<String, Object> stats = new LinkedHashMap();
-		MQQueue queue = null;
-		ArrayList<String> queuelist  = new ArrayList<String>();
-	    queuelist = listOfQueue();
-
-	    String queueName ="";
-		for (int i = 0; i < queuelist.size(); i++)
-		{
-		try {
-					
-    		queueName = queuelist.get(i);
-			queue = qmgr.accessQueue(queueName, MQC.MQOO_INQUIRE | MQC.MQOO_INPUT_AS_Q_DEF, null, null, null);
-            stats.put("qName", queueName);
-			stats.put("Description", queue.getDescription().trim());
-			stats.put("CurrentDepth", new Integer(queue.getCurrentDepth()));
-			stats.put("OpenOutputCount", new Integer(queue.getOpenOutputCount()));
-			stats.put("OpenInputCount", new Integer(queue.getOpenInputCount()));
-
-			if (queue.getInhibitGet() == MQC.MQQA_GET_INHIBITED) {
-				stats.put("InhibitGet", Boolean.TRUE);
-			} else {
-				stats.put("InhibitGet", Boolean.FALSE);
-			}
-
-			if (queue.getInhibitPut() == MQC.MQQA_PUT_INHIBITED) {
-				stats.put("InhibitPut", Boolean.TRUE);
-			} else {
-				stats.put("InhibitPut", Boolean.FALSE);
-			}
-
-			if (queue.getShareability() == MQC.MQQA_SHAREABLE) {
-				stats.put("Sharable", Boolean.TRUE);
-			} else {
-				stats.put("Sharable", Boolean.FALSE);
-			}
-
-			if (queue.getTriggerControl() == MQC.MQTC_ON) {
-				stats.put("TriggerControl", Boolean.TRUE);
-				stats.put("TriggerData", queue.getTriggerData());
-				stats.put("TriggerDepth", new Integer(queue.getTriggerDepth()));
-				stats.put("TriggerMessagePriority", new Integer(queue.getTriggerMessagePriority()));
-
-				switch (queue.getTriggerType()) {
-				case MQC.MQTT_NONE:
-					stats.put("TriggerType", "None");
-					break;
-
-				case MQC.MQTT_DEPTH:
-					stats.put("TriggerType", "Depth");
-					break;
-
-				case MQC.MQTT_EVERY:
-					stats.put("TriggerType", "Every");
-					break;
-
-				case MQC.MQTT_FIRST:
-					stats.put("TriggerType", "First");
-					break;
-
-				default:
-					stats.put("TriggerType", "Unknown");
-				}
-			} else {
-				stats.put("TriggerControl", Boolean.FALSE);
-			}
-
-			stats.put("MaximumDepth", new Integer(queue.getMaximumDepth()));
-			stats.put("MaximumMessageLength", new Integer(queue.getMaximumMessageLength()));
-			}
-		catch (MQException ex) {
-			if (ex.reasonCode != 2033) {
-				ex.printStackTrace();
-				System.out.println("its not a 2033 error");
-			} else {
-				System.out.println("PCF calls gave a 2033 reason code, ignoring");
-			}
-		} catch (Exception ex) {
-			throw new Exception(ex);
-		} finally {
-			if (queue != null) {
-				try {
-					queue.close();
-				} catch (MQException ex) {
-					System.out.println("ignoring error closing queue: ");
-				}
-			}
-			}
-		}
-
-		System.out.println("stats output");
-
-		Iterator it = stats.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pairs = (Map.Entry) it.next();
-			System.out.println(pairs.getKey() + " = " + pairs.getValue());
-		}
-
-		return stats;
 	}*/
 	
 	public synchronized Map getStatistics(String queueName) throws Exception {
@@ -172,11 +73,25 @@ public class QueueManager {
 		try {
 		    //queueName = queuelist.get(i);
 		queue = qmgr.accessQueue(queueName, MQC.MQOO_INQUIRE | MQC.MQOO_INPUT_AS_Q_DEF, null, null, null);
+		
 		            stats.put("qName", queueName);
 		stats.put("Description", queue.getDescription().trim());
+		if(queue.getQueueType()==MQC.MQQT_LOCAL) {
 		stats.put("CurrentDepth", new Integer(queue.getCurrentDepth()));
 		stats.put("OpenOutputCount", new Integer(queue.getOpenOutputCount()));
 		stats.put("OpenInputCount", new Integer(queue.getOpenInputCount()));
+		stats.put("MaximumDepth", new Integer(queue.getMaximumDepth()));
+		stats.put("MaximumMessageLength", new Integer(queue.getMaximumMessageLength()));
+
+		}
+		else {
+			
+			stats.put("CurrentDepth", "Not Applicabel for this queue");
+			stats.put("OpenInputCount", "Not Applicabel for this queue");
+			stats.put("MaximumDepth", "Not Applicabel for this queue");
+			stats.put("MaximumMessageLength", "Not Applicabel for this queue");
+
+			}
 
 		if (queue.getInhibitGet() == MQC.MQQA_GET_INHIBITED) {
 		stats.put("InhibitGet", Boolean.TRUE);
@@ -226,8 +141,6 @@ public class QueueManager {
 		stats.put("TriggerControl", Boolean.FALSE);
 		}
 
-		stats.put("MaximumDepth", new Integer(queue.getMaximumDepth()));
-		stats.put("MaximumMessageLength", new Integer(queue.getMaximumMessageLength()));
 		}
 		catch (MQException ex) {
 		if (ex.reasonCode != 2033) {
@@ -267,7 +180,7 @@ public class QueueManager {
 		
 		final Map<String, String> channel = new LinkedHashMap();
 
-		PCFMessageAgent agent = new PCFMessageAgent("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN");
+		PCFMessageAgent agent = new PCFMessageAgent(host, port, this.channel);
 
 		PCFMessage request = new PCFMessage(CMQCFC.MQCMD_INQUIRE_CHANNEL_STATUS);
 
@@ -335,7 +248,7 @@ public class QueueManager {
 		try
 
 		{
-			PCFMessageAgent agent = new PCFMessageAgent("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN");
+			PCFMessageAgent agent = new PCFMessageAgent(host, port, channel);
 
 			PCFMessage request = new PCFMessage(CMQCFC.MQCMD_INQUIRE_Q_NAMES);
 
@@ -386,6 +299,67 @@ public class QueueManager {
 
 	}
 
+	public ArrayList<String> listOfLocalQueues() throws MQException
+
+	{
+
+		System.out.println("entering");
+		ArrayList<String> queuelist  = new ArrayList<String>();
+		try
+
+		{
+			PCFMessageAgent agent = new PCFMessageAgent(host, port, channel);
+
+			PCFMessage request = new PCFMessage(CMQCFC.MQCMD_INQUIRE_Q_NAMES);
+
+			request.addParameter(CMQC.MQCA_Q_NAME, "*");
+
+			request.addParameter(CMQC.MQIA_Q_TYPE, MQC.MQQT_LOCAL);
+
+			PCFMessage[] responses = agent.send(request);
+
+			String[] names = (String[]) responses[0].getParameterValue(CMQCFC.MQCACF_Q_NAMES);	
+			
+			for (int i = 0; i < names.length; i++)
+
+			{
+				names[i] = names[i].trim();
+				if (names[i].startsWith("AMQ"))
+				continue;
+				queuelist.add(names[i]);
+				
+				
+			}
+
+		}
+
+		catch (PCFException pcfe)
+
+		{
+
+			System.err.println("PCF error: " + pcfe);
+
+		}
+
+		catch (MQException mqe)
+
+		{
+
+			System.err.println(mqe);
+
+		}
+
+		catch (IOException ioe)
+
+		{
+
+			System.err.println(ioe);
+
+		}
+		return queuelist;
+
+	}
+	
 	public ArrayList<String> listOfAliasQueue() throws MQException
 
 	{
@@ -393,7 +367,7 @@ public class QueueManager {
 		try
 
 		{
-			PCFMessageAgent agent = new PCFMessageAgent("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN");
+			PCFMessageAgent agent = new PCFMessageAgent(host, port, channel);
 
 			PCFMessage request = new PCFMessage(CMQCFC.MQCMD_INQUIRE_Q_NAMES);
 
@@ -447,103 +421,7 @@ public class QueueManager {
 		return queuelist;
 
 	}
-/*
-	public String channelRestart(String chnlName) throws MQException
-	{
-	String checkStatus = "";
-	PCFAgent pcfAgent = null;
-	String channelName = chnlName;
 
-
-	PCFMessageAgent agent = new PCFMessageAgent("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN");
-
-
-	// build a request
-
-	//PCFMessage request = new PCFMessage (CMQCFC.MQCMD_INQUIRE_Q_NAMES);
-	PCFMessage request = new PCFMessage(CMQCFC.MQCMD_INQUIRE_CHANNEL_STATUS);
-	PCFMessage request2 = new PCFMessage(CMQCFC.MQCMD_INQUIRE_CHANNEL_STATUS);
-	// add a parameter designating the name of the channel for which status
-	// is requested
-
-	request.addParameter(CMQCFC.MQCACH_CHANNEL_NAME, "*");
-
-	// add a parameter designating the instance type (current) desired
-
-	request.addParameter(CMQCFC.MQIACH_CHANNEL_INSTANCE_TYPE, CMQC.MQOT_CURRENT_CHANNEL);
-
-	PCFMessage[] responses;
-
-	try {
-
-
-	responses = agent.send(request);
-
-	for (int j = 0; j < responses.length; j++) {
-
-	// get the channel name and trim the spaces
-
-	String temp = "";
-
-	temp = responses[j].getStringParameterValue(CMQCFC.MQCACH_CHANNEL_NAME);
-
-	channelName = temp.trim();
-
-	// System.out.println(channelName);
-
-	int chlStatus = responses[j].getIntParameterValue(CMQCFC.MQIACH_CHANNEL_STATUS);
-
-	System.out.println("channel status: " + chlStatus);
-
-	String[] chStatusText = {
-
-	"", "BINDING", "STARTING", "RUNNING/INACTIVE",
-
-	"STOPPING", "RETRYING", "STOPPED",
-
-	"REQUESTING", "PAUSED",
-
-	"", "", "", "", "INITIALIZING"
-
-	};
-
-	if(chlStatus!=3)
-	{
-	PCFMessage response;
-	PCFParameter [] parameters = new PCFParameter [] {
-	new MQCFST (CMQCFC.MQCACH_CHANNEL_NAME, channelName),
-	};
-	//PCFMessage   request2 = new PCFMessage (CMQCFC.MQCMD_START_CHANNEL);
-	//request2.addParameter(CMQCFC.MQCMD_START_CHANNEL,channelName);
-	//responses = agent.send (request2);
-	MQMessage [] pcfResponses = agent.send (CMQCFC.MQCMD_START_CHANNEL, 
-	parameters);
-	response = new PCFMessage(pcfResponses[0]);
-	//System.out.println(pcfResponses.toString());
-
-	}
-
-	checkStatus = chStatusText[chlStatus];
-
-	System.out.println("chl: " + channelName + " STATUS: " + checkStatus);
-
-	}
-
-
-	}
-
-	catch (IOException e) {
-
-	// TODO Auto-generated catch block
-
-	e.printStackTrace();
-
-	}
-	
-	return "SUCCESS";
-
-	}
-*/
 	public int getQueuePutStatus(String queueName) throws MQException, Exception
 	{
 		int flag = 0 ;
@@ -632,11 +510,11 @@ public class QueueManager {
 	{
 	if (queueManager == null)
 	{
-	MQEnvironment.channel = "MQPREPRDSUP.SVRCONN";
+	MQEnvironment.channel = channel;
 
-	MQEnvironment.port = 1430;
+	MQEnvironment.port = port;
 
-	MQEnvironment.hostname = "unxs0614.ghanp.kfplc.com";
+	MQEnvironment.hostname = host;
 	System.out.println(MQEnvironment.hostname);
 
 	MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES);
@@ -706,7 +584,7 @@ public class QueueManager {
 	String channelName = "";
 
 
-	PCFMessageAgent agent = new PCFMessageAgent("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN");
+	PCFMessageAgent agent = new PCFMessageAgent(host, port, channel);
 
 
 	// build a request
@@ -784,7 +662,7 @@ public class QueueManager {
 	public String channelRestart(String channelName) throws MQException, IOException{
 
 
-	PCFMessageAgent agent = new PCFMessageAgent("unxs0614.ghanp.kfplc.com", 1430, "MQPREPRDSUP.SVRCONN");
+	PCFMessageAgent agent = new PCFMessageAgent(host, port, channel);
 	PCFMessage response;
 	PCFParameter [] parameters = new PCFParameter [] {
 	new MQCFST (CMQCFC.MQCACH_CHANNEL_NAME, channelName),
@@ -800,6 +678,145 @@ public class QueueManager {
 
 	return "SUCCESS";
 	}
+
+	
+	public JSONArray listOfQueues() throws MQException
+
+	{
+
+		System.out.println("entering listOfQueues method");
+		JSONArray queuelist  = new JSONArray();
+		JSONObject queueStats=new JSONObject();
+		try
+
+		{
+			PCFMessageAgent agent = new PCFMessageAgent(host, port, channel);
+
+			PCFMessage request = new PCFMessage(CMQCFC.MQCMD_INQUIRE_Q_NAMES);
+
+			request.addParameter(CMQC.MQCA_Q_NAME, "*");
+
+			//request.addParameter(CMQC.MQIA_Q_TYPE, MQC.MQQT_LOCAL);
+
+			PCFMessage[] responses = agent.send(request);
+
+			String[] names = (String[]) responses[0].getParameterValue(CMQCFC.MQCACF_Q_NAMES);	
+			
+			for (int i = 0; i < names.length; i++)
+
+			{
+				names[i] = names[i].trim();
+				if (names[i].startsWith("AMQ"))
+				continue;
+				try {
+				queueStats = getQueueStatistics(names[i]);
+				if(!queueStats.isEmpty()) {
+				queuelist.add(queueStats);}
+				
+				}
+				catch(Exception e) {
+					System.err.println(e);
+				}
+
+			}
+
+		}
+
+		catch (PCFException pcfe)
+
+		{
+
+			System.err.println("PCF error: " + pcfe);
+
+		}
+
+		catch (MQException mqe)
+
+		{
+
+			System.err.println(mqe);
+
+		}
+
+		catch (IOException ioe)
+
+		{
+
+			System.err.println(ioe);
+
+		}
+		return queuelist;
+
+	}
+	
+	public JSONObject getQueueStatistics(String queueName) throws Exception {
+		// System.out.println("inside");
+		MQQueue queue = null;
+		JSONObject stats  = new JSONObject();
+		    
+		try {
+			queue = qmgr.accessQueue(queueName, MQC.MQOO_INQUIRE | MQC.MQOO_INPUT_AS_Q_DEF, null, null, null);
+		
+			stats.put("qName", queueName);
+			//stats.put("Description", queue.getDescription().trim());
+			if(queue.getQueueType()==MQC.MQQT_LOCAL) {
+				stats.put("CurrentDepth", new Integer(queue.getCurrentDepth()));
+				stats.put("OpenInputCount", new Integer(queue.getOpenInputCount()));
+				stats.put("MaximumDepth", new Integer(queue.getMaximumDepth()));
+				stats.put("MaximumMessageLength", new Integer(queue.getMaximumMessageLength()));
+			}else {
+				stats.put("CurrentDepth", "Not Applicable");
+				stats.put("OpenInputCount", "Not Applicable");
+				stats.put("MaximumDepth", "Not Applicable");
+				stats.put("MaximumMessageLength", "Not Applicable");
+			}
+	
+			if(queue.getQueueType()==MQC.MQQT_LOCAL || queue.getQueueType()==MQC.MQQT_ALIAS) { 
+				if (queue.getInhibitGet() == MQC.MQQA_GET_INHIBITED) {
+				stats.put("InhibitGet", "Yes");
+				} else {
+				stats.put("InhibitGet", "No");
+				}
+			}else {
+				stats.put("InhibitGet", "Not Applicable");
+			}
+	
+			if (queue.getInhibitPut() == MQC.MQQA_PUT_INHIBITED) {
+			stats.put("InhibitPut", "Yes");
+			} else {
+			stats.put("InhibitPut", "No");
+			}
+	
+		}
+		catch (MQException ex) {
+			if (ex.reasonCode != 2033) {
+			ex.printStackTrace();
+			System.out.println("its not a 2033 error");
+			} else {
+			System.out.println("PCF calls gave a 2033 reason code, ignoring");
+			}
+		} catch (Exception ex) {
+			throw new Exception(ex);
+		} finally {
+			if (queue != null) {
+			try {
+			queue.close();
+			} catch (MQException ex) {
+			System.out.println("ignoring error closing queue: ");
+			}
+			}
+		}
+
+		System.out.println("stats output");
+
+		Iterator it = stats.entrySet().iterator();
+		while (it.hasNext()) {
+		Map.Entry pairs = (Map.Entry) it.next();
+		System.out.println(pairs.getKey() + " = " + pairs.getValue());
+		}
+
+		return stats;
+		}
 
 
 }
